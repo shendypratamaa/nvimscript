4577363c3209975bbf85ec06d51450e239cd9cc7
shendy pratama <shendypratamaa@gmail.com>
2021-12-23 23:03:15 +0700

    remove status gitbranch and changes to filesize information

--- themes/lualine.lua
+++ themes/lualine.lua
@@ -6,7 +6,7 @@ require('lualine').setup {
   },
   sections = {
     lualine_a = {'mode'},
-    lualine_b = {'branch'},
+    lualine_b = {'filesize'},
     lualine_c = {'filename'},
     lualine_x = {'encoding', 'fileformat', 'filetype'},
     lualine_y = {'progress'},
